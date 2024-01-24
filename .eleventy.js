f8c665cca9fbc84a71bcc3db8237007a0a71ec5a
ZSchleger <zachs376@gmail.com>
2024-01-24 18:07:27 -0500

    Updates

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function(eleventyConfig) {
+    // Return your Object options:
+    return {
+        dir: {
+            input: "source",
+            output: "_site"
+        }
+    }
+};
