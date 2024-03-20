3fd7b0763f41d73713f4a67f5c22778c28fdc0ed
ZSchleger <zachs376@gmail.com>
2024-03-20 13:37:50 -0400

    Automated Buttons

---
+++ source/_data/authors.js
@@ -0,0 +1,18 @@
+const poemAttributes = require('./poemAttributes.json');
+
+module.exports = () => {
+    let authorsSet = new Set(poemAttributes.map(poem => poem.author));
+    let authors = Array.from(authorsSet).map(author => ({
+        original: author,
+        withSpaces: author.replace(/([A-Z])/g, ' $1')
+    }));
+
+
+    authors.sort((a, b) => {
+        let aLastName = a.withSpaces.split(" ").pop();
+        let bLastName = b.withSpaces.split(" ").pop();
+        return aLastName.localeCompare(bLastName);
+    });
+
+    return authors;
+};
